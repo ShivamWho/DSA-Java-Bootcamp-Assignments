ed2f3c351e11aa96d24f2ef8bd443e2143cb10a2
ShivamWho <shivam2094.be20@chitkara.edu.in>
2021-08-20 00:12:33 +0530

    Array lecture complete

---
+++ Lectures/Introduction to Arrays and ArrayList in Java/src/com/company/Max.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 29, 77, 45, 10};
+
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr, 0, 2));
+    }
+
+    static int max(int[] arr) {
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        int maxVal = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    static int maxRange(int[] arr, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        if (arr == null) {
+            return -1;
+        }
+
+        int maxVal = arr[start];
+        for (int i = start; i < end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
---
+++ Lectures/Introduction to Arrays and ArrayList in Java/src/com/company/SwapInArrays.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class SwapInArrays {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        swap(arr, 1, 3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
